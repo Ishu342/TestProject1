35466888bd49bab22ad7b01499a2d280578e11b9
Gouthami <Gouthami@DESKTOP-5O1STAS>
2021-07-13 11:53:34 +0530

    commit restAPIAdd project files.

---
+++ TestProject1/ApiHelper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using System.IO;
+using Flurl;
+using RestSharp.Authenticators;
+
+namespace TestProject1
+{
+    class ApiHelper<T>
+    {
+
+        public IRestClient restClient;
+        public IRestRequest RestRequest;
+        public string baseurl = "https://reqres.in";
+
+        public IRestClient SetUrl(string sourceUrl)
+        {
+            var url = Url.Combine(baseurl, sourceUrl);
+            var restClient = new RestClient(url);
+            restClient.Authenticator = new HttpBasicAuthenticator("admin", "welcome");
+            return restClient;
+
+        }
+
+        public IRestRequest CreatePostRequest(string jsonStrg)
+        {
+            IRestRequest RestRequest = new RestRequest(Method.POST);
+            RestRequest.AddHeader("Accept", "application/json");
+            RestRequest.AddParameter("application/json", jsonStrg, ParameterType.RequestBody);
+            return RestRequest;
+
+        }
+
+
+        public IRestRequest CreatePutRequest(string jsonstr)
+        {
+            IRestRequest RestRequest = new RestRequest(Method.PUT);
+            RestRequest.AddHeader("Accept", "application/json");
+            RestRequest.AddParameter("application/json", jsonstr, ParameterType.RequestBody);
+            return RestRequest;
+
+        }
+        public IRestRequest CreateGetRequest()
+        {
+            IRestRequest RestRequest = new RestRequest(Method.GET);
+            RestRequest.AddHeader("Accept", "application/json");            
+            return RestRequest;
+
+        }
+        public IRestRequest CreateDeleteRequest()
+        {
+            IRestRequest RestRequest = new RestRequest(Method.DELETE);
+            RestRequest.AddHeader("Accept", "application/json");
+            return RestRequest;
+
+        }
+
+        public IRestResponse GetResponse(RestClient restClient, IRestRequest RestRequest)
+        {
+            return restClient.Execute(RestRequest);
+
+        }
+
+        public DataTableObject GetContent<DataTableObject>(IRestResponse restResponse)
+        {
+            var content = restResponse.Content;
+            DataTableObject deserializeobject = Newtonsoft.Json.JsonConvert.DeserializeObject< DataTableObject>(content);
+            return deserializeobject;
+
+        }
+
+        
+    }
+}
---
+++ TestProject1/CreateUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject1
+{
+    class CreateUser
+    {
+       
+            public string name { get; set; }
+            public string job { get; set; }
+            public string id { get; set; }
+            public DateTime createdAt { get; set; }
+       
+    }
+}
